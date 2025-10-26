10c6e3d62e59b37977fbe58c29a9aa01db5063e0
Anil Oeguetveren <anil.oeguetveren@mercedes-benz.com>
2025-10-26 19:36:14 +0100

    Upload pdf is fixed

--- src/main/java/com/aitools/jobsearcher/controller/ChatController.java
+++ src/main/java/com/aitools/jobsearcher/controller/ChatController.java
@@ -7,9 +7,7 @@ import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class ChatController {
